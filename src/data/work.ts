50333ac6c84552acce9a0a924390a687aff1f306
shashank-poola <shashankpoola123@gmail.com>
2026-04-01 02:29:03 +0530

    feat: added work experience

--- src/data/work.ts
+++ src/data/work.ts
@@ -1,13 +1,25 @@
 export const WORK_DATA = [
   {
+    company: "100xdevs",
+    href: "https://100xdevs.com",
+    badges: [],
+    location: "Remote",
+    title: "Full stack developer",
+    logoUrl: "/100xdevs.jpg",
+    start: "Sep 2025",
+    end: "Mar 2026",
+    description:
+      ""
+  },
+  {
     company: "Freelance",
     href: "https://shasha.ink",
     badges: [],
     location: "Remote",
-    title: "Full stack developer",
-    logoUrl: "/freelance.jpg",
-    start: "March 2025",
-    end: "January 2026",
+    title: "Contract Full stack developer",
+    logoUrl: "/stealth.jpg",
+    start: "Mar 2025",
+    end: "Present",
     description:
       "• Built and delivered end-to-end product features for Monex Builder and Reframe Studio as a freelance full-stack developer\n• Created React + TypeScript multi-page applications with a strong focus on UX, performance, and maintainability\n• Designed scalable data pipelines handling 2K+ daily events, boosting model effectiveness by 40%"
   }
